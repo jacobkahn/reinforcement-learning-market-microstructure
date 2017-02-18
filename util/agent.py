1d11460786888dfad0e90e9ecb68ad18744d4ec5
Amit Patel <amitvpatel06@gmail.com>
2017-02-18 16:52:22 -0500

    more work on DP, filled out base case of table

--- util/agent.py
+++ util/agent.py
@@ -1,36 +1,43 @@
 from environment import *
 from collections import defaultdict
 
-def dp_algo(ob_file, H, V, I, T):
+def dp_algo(ob_file, H, V, I, T, L):
 	c = {}
 	env = Environment(ob_file, setup=False)
 	steps = H / T
+	vol_unit = V / I
 	# state variables
 	t = T
 	inv = V
 	market_cost = 999999
 	volume_misbalance = 0
 	# loop for the DP algorithm
-	for i in range(0, T)[::-1]:
+	for t in range(0, T+1)[::-1]:
 		# regenerate the order books
-		for a in range(0, I):
-			env.get_timesteps(i, T)
-			for j in range(i, T):
-				curr_book = env.get_next_state()
-				market_cost = compute_mc(curr_book)
-				volume_misbalance = compute_vm(curr_book)		
-				key = str(t) + "," +str(inv)+ "," +str(market_cost) + "," +str(volume_misbalance)  
-				if j == T:
-					if key not in c:
-						c[key] = {}
+		env.get_timesteps(t, T+1)
+		curr_book = env.get_next_state()
+		import	pdb
+		pdb.set_trace()
+		#market_cost = compute_mc(curr_book)
+		#volume_misbalance = compute_vm(curr_book)
+		market_cost = 17
+		volume_misbalance = 17		
+		actions = sorted(curr_book.b.keys())
+		for i in range(0, I + 1):
+			for a in range(0, L + 1):
+				curr_units = int(V/vol_unit) * i
+				key = str(t) + "," + str(curr_units)+ "," +str(market_cost) + "," +str(volume_misbalance) 
+				if t is T:
+					spent, leftover = env.limit_order(1, 9999999, vol_unit * i)
+					if leftover	>= 0:
+						spent += leftover * actions[-1]
+					c[key] = {}
+					c[key][a] = spent
+					print c
+					break
 
 
-
-
-			
-
-
-
+					
 			
 if __name__ == "__main__":
-	dp_algo("../LOBSTER_SampleFile_MSFT_2012-06-21_1/MSFT_2012-06-21_34200000_57600000_orderbook_1.csv", 10, 10000, 10, 10	)
+	dp_algo("../LOBSTER_SampleFile_MSFT_2012-06-21_1/MSFT_2012-06-21_34200000_57600000_orderbook_1.csv", 10, 10000, 10, 10, 5)
